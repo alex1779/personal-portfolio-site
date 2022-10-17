401adf1635ed6b1bc258e45ef79b6c6656bdd490
alex1779 <alex1779@gmail.com>
2022-10-17 16:16:50 -0300

    Adding Javascript file.

---
+++ js/index.js
@@ -0,0 +1,7 @@
+let menuItem = document.querySelector('#icon-button-button');
+let logo = document.querySelector('#div-logo-a');
+
+function myFunction() {
+  document.getElementById("div-logo-a").style.display = "none";
+
+}
